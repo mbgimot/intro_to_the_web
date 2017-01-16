a832b5902257421ab9d28bdde3d481b0231bbb05
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-16 15:54:55 +0000

    added shotgun gem and secured session data

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   'Hello world!'
+end
+
+get '/secret' do
+  "What's a good time to play table tennis?"
+end
+
+get '/tennis' do
+  "i reckon 5pm is a decent shout"
+end
+
+get '/agree' do
+  "sam agrees"
+end
+
+get '/agree2' do
+  "serjey agrees"
+end
+
+get '/agree3' do
+  "mitch agrees"
 end
